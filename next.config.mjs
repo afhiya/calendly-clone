3f858e6aa0b1743608048a522cf86527dfb3860a
afhiya <faridrakapratama@gmail.com>
2024-12-13 21:09:34 +0700

    fix:punycode and date-fns-tz

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { staleTimes: { dynamic: 0 } },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      punycode: false,
+      "date-fns-tz": false
+    };
+  }
 };
 
 export default nextConfig;
