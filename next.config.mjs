d381a661a25c87cba8ae7e557c92e59b522ee7d6
afhiya <faridrakapratama@gmail.com>
2024-12-14 08:18:26 +0700

    fix-production: Try not experimental

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { staleTimes: { dynamic: 0 } },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       punycode: false,
       "date-fns-tz": false
     };
+    return config
   }
 };
 
